d42564c344f9a6cf3758a49a69d5a39b1abb8642
Brian <brianmcwilliams15@gmail.com>
2021-02-28 10:11:23 -0500

    Add a few comments for the programming example

--- CyberHunters/Assets/Scripts/ProgrammingBasics/ProgrammingBasics.cs
+++ CyberHunters/Assets/Scripts/ProgrammingBasics/ProgrammingBasics.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ProgrammingBasics : MonoBehaviour
 {
-    public TextMesh textMesh;
+    public TextMesh textMesh;             //These are variables, use them to modify the text you see on screen
     public TextMesh timerTextMesh;
     // Start is called before the first frame update
     void Start()
     {
-        string myMessageForTheConsole = "Write something here";
+        //These variables we are going to use for storing our messages, try using these with Debug.Log()
+        string myMessageForTheConsole = "Write something here";    
         string myTextForTheScreen = "Text to put on screen";
 
         Debug.Log("Hello World");
+
+        //You can use textMesh.text and textMesh.color to modify the text and color you see on screen;
+        //Keep in mind there are 2 textMesh variables, pay attention to which one you are modifying
 
         //textMesh.text = "I love bagels";
         //textMesh.color = new Color(1.0f, 0.1f, 0.2f);  //Red,Green,Blue
