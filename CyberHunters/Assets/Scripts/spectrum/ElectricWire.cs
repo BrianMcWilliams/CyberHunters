bef9ef27b151b3cab1661bf31f4258b82117b21c
celia <celiashi666@hotmail.com>
2021-03-07 12:32:20 -0500

    new script and picture (ElectricWire)

---
+++ CyberHunters/Assets/Scripts/spectrum/ElectricWire.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricWire : MonoBehaviour
+{
+    public GameObject buttonPickUp;
+    void Start()
+    {
+        buttonPickUp.SetActive(false);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject collisionObject = collision.gameObject;
+        PlayerMovement mouvementComponant = gameObject.GetComponent<PlayerMovement>();
+        if (mouvementComponant == null)
+        {
+            return;
+        }
+        else
+        {
+            buttonPickUp.SetActive(true);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        GameObject collisionObject = collision.gameObject;
+        PlayerMovement mouvementComponant = gameObject.GetComponent<PlayerMovement>();
+        if (mouvementComponant == null)
+        {
+            return;
+        }
+        else
+        {
+            buttonPickUp.SetActive(false);
+        }
+    }
+}
