d544e8dacccf154b2dfe8c07947d1c12c48318cc
Alison <alisonquato@gmail.com>
2021-03-21 11:48:45 -0400

    jumpbutton

--- CyberHunters/Assets/Scripts/spectrum/pickUp.cs
+++ CyberHunters/Assets/Scripts/spectrum/pickUp.cs
@@ -7,17 +7,31 @@ public class pickUp : MonoBehaviour
     public GameObject button;
     public GameObject wire;
     public GameObject dialog;
+    public GameObject jumpButton;
 
     void OnCollisionEnter2D (Collision2D other)
     {
         button.SetActive (true); 
-        dialog.SetActive(true);
+        
+        if(dialog != null)
+        {
+            dialog.SetActive(true);
+        }
+        
+        jumpButton.SetActive (false);
+    }
+
+    void OnCollisionExit2D (Collision2D other) 
+    {
+        button.SetActive (false); 
+        jumpButton.SetActive (true);
     }
 
     public void clickThisButton()
     {
        wire.SetActive (false);
        button.SetActive (false);
+       jumpButton.SetActive (true);
     }
     
 }
