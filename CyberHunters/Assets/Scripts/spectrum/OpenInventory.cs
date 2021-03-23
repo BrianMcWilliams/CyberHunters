08669db19e18851fd7ff2c3dad1c5e9880b7c059
Alison <alisonquato@gmail.com>
2021-03-22 21:56:53 -0400

    Inventory, pickUp

---
+++ CyberHunters/Assets/Scripts/spectrum/OpenInventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenInventory : MonoBehaviour
+{
+    public GameObject openButton;
+    public GameObject exitButton;
+
+    public GameObject inventory;
+
+    public void clickThisButton()
+    {
+        exitButton.SetActive(true);
+        inventory.SetActive (true);
+        openButton.SetActive (false);
+    } 
+
+    public void clickedThisButton()
+    {
+        exitButton.SetActive(false);
+        inventory.SetActive(false);
+        openButton.SetActive(true);
+    }
+
+
+
+}
--- CyberHunters/Assets/Scripts/spectrum/pickUp.cs
+++ CyberHunters/Assets/Scripts/spectrum/pickUp.cs
@@ -2,23 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pickUp : MonoBehaviour
-{
+public class pickUp : MonoBehaviour {
+
+    private Inventory inventory;
+    public GameObject itemButton;
     public GameObject button;
     public GameObject wire;
     public GameObject dialog;
     public GameObject jumpButton;
 
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
     void OnCollisionEnter2D (Collision2D other)
     {
         button.SetActive (true); 
-        
+
         if(dialog != null)
         {
             dialog.SetActive(true);
         }
         
         jumpButton.SetActive (false);
+
     }
 
     void OnCollisionExit2D (Collision2D other) 
@@ -32,6 +41,19 @@ public class pickUp : MonoBehaviour
        wire.SetActive (false);
        button.SetActive (false);
        jumpButton.SetActive (true);
+
+       for (int i = 0; i < inventory.slots.Length; i++)      
+       {  
+            if (inventory.isFull[i] == false) 
+            { 
+                inventory.isFull[i] = true; 
+                Instantiate(itemButton, inventory.slots[i].transform, false); 
+                break;
+                
+            }
+        }
+
+
     }
     
 }
