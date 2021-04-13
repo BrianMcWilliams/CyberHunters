06be67753226e95c4aeb956a6eb87c98a24244f6
Alison <alisonquato@gmail.com>
2021-04-12 20:02:04 -0400

    dialog

---
+++ CyberHunters/Assets/_SPECTRUM/scripts/OpenDoor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject door;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        door.SetActive (true);
+    }
+
+}
